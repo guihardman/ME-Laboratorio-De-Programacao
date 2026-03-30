e411f322d3a2727aa6dc328177449beabdafac65
rick Cris <erickcris45@gmail.com>
2026-03-29 23:28:59 -0300

    comentarios atualizados!

--- test/test_inventario.py
+++ test/test_inventario.py
@@ -1,16 +1,18 @@
-
 import unittest
 
+# Importar funções
 from modulos.inventario import adicionar_item, remover_item, usar_pocao_cura
 
 
 class TestInventario(unittest.TestCase):
 
+    #"teste de adicionar item"
     def test_adicionar_item(self):
         mochila = []
         adicionar_item(mochila, "Pocao de Vida")
         self.assertIn("Pocao de Vida", mochila)
 
+    #"teste de remover item"
     def test_remover_item_com_sucesso(self):
         mochila = ["Espada", "Escudo"]
         remover_item(mochila, "Espada")
@@ -21,6 +23,7 @@ class TestInventario(unittest.TestCase):
         with self.assertRaises(ValueError):
             remover_item(mochila, "Chave")
 
+    #"testes de cura"
     def test_curar_personagem_normal(self):
         heroi = {'hp_atual': 50, 'hp_maximo': 100}
         usar_pocao_cura(heroi, 30)
@@ -35,7 +38,7 @@ class TestInventario(unittest.TestCase):
         heroi = {'hp_atual': 0, 'hp_maximo': 100}
         with self.assertRaises(ValueError):
             usar_pocao_cura(heroi, 50)
-"excluir depois"
+
 
 if __name__ == '__main__':
     unittest.main()
